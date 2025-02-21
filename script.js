64029cc91ea476b80bbb727989bb05e99ac9650d
max613 <vega14th@naver.com>
2025-02-21 12:27:57 +0900

    전체 수정  	new file:   Mainpage.html  	new file:   header.css  	new file:   header.html  	modified:   join.html  	renamed:    "\353\241\234\352\267\270\354\235\270.html" -> login.html  	new file:   script.js  	deleted:    "\353\251\224\354\235\270\355\216\230\354\235\264\354\247\200.html"  	deleted:    "\355\232\214\354\233\220\352\260\200\354\236\205.html"

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  let loginstatus = sessionStorage.getItem("loginstatus");
+  let name = localStorage.getItem("name");
+
+  // header.html 로드 후 로그인 상태 확인
+  $("#header").load("header.html", function () {
+    if (loginstatus === "true") {
+      // 로그인 상태일 때
+      $("#login, #join").hide(); // 로그인 & 회원가입 버튼 숨김
+      $("#welcome-text").text(`${name}님, 환영합니다!`).show(); // 환영 메시지 표시
+      $("#logout").show(); // 로그아웃 버튼 표시
+    } else {
+      // 로그아웃 상태일 때
+      $("#login, #join").show(); // 로그인 & 회원가입 버튼 표시
+      $("#welcome-text, #logout").hide(); // 환영 메시지 & 로그아웃 버튼 숨김
+    }
+  });
+
+  // 로그아웃 버튼 클릭 이벤트
+  $(document).on("click", "#logout", function () {
+    sessionStorage.setItem("loginstatus", "false");
+    alert("로그아웃되었습니다.");
+    window.location.href = "Mainpage.html";
+  });
+});
+function loginCheck() {
+  let loginstatus = sessionStorage.getItem("loginstatus") === "true";
+  if (loginstatus == true) {
+    window.location.href = "page1.html";
+  } else {
+    alert("로그인을 해주세요.");
+  }
+}
