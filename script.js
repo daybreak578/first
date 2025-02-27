a7d12736879d5c68cda8205d4340da18e3423606
김태완 <vega14th@naver.com>
2025-02-27 10:00:06 +0900

    헤더부분 수정  	modified:   Home.html  	deleted:    aasdf.html  	modified:   header.css  	modified:   header.html  	new file:   headerhome.html  	modified:   script.js  	deleted:    "\353\224\224\354\236\220\354\235\270.html"

--- script.js
+++ script.js
@@ -15,6 +15,18 @@ $(document).ready(function () {
       $("#welcome-text, #logout").hide(); // 환영 메시지 & 로그아웃 버튼 숨김
     }
   });
+  $("#headerhome").load("headerhome.html", function () {
+    if (loginstatus === "true") {
+      // 로그인 상태일 때
+      $("#login, #join").hide(); // 로그인 & 회원가입 버튼 숨김
+      $("#welcome-text").text(`${name}님, 환영합니다!`).show(); // 환영 메시지 표시
+      $("#logout").show(); // 로그아웃 버튼 표시
+    } else {
+      // 로그아웃 상태일 때
+      $("#login, #join").show(); // 로그인 & 회원가입 버튼 표시
+      $("#welcome-text, #logout").hide(); // 환영 메시지 & 로그아웃 버튼 숨김
+    }
+  });
 
   // 로그아웃 버튼 클릭 이벤트
   $(document).on("click", "#logout", function () {
@@ -32,12 +44,12 @@ function loginCheck(name) {
   }
 }
 function move(name) {
-    window.location.href = `${name}.html`;
+  window.location.href = `${name}.html`;
 }
 function loginChecka() {
   let loginstatus = sessionStorage.getItem("loginstatus") === "true";
   if (loginstatus != true) {
     alert("로그인을 해주세요.");
     window.location.href = "Home.html";
-  } 
+  }
 }
